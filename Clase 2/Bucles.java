825d75033d83f82f3b2d086fdc926865d24a0ab2
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-02-14 01:14:07 +0000

    Bucles, creacion y practicas

---
+++ Clase 2/Bucles.java
@@ -0,0 +1,8 @@
+public class Bucles {
+    public static void main(String[] args) {
+        // For(Incio; condicion de parada; incremento o decremento)
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Número de iteración = " + i );
+        }
+    }
+}

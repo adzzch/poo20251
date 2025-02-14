a0513be2364bd7eebe3db7d5e2e331b09f189c80
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-02-14 01:29:12 +0000

    Correción de llaves

--- Clase 2/Bucles.java
+++ Clase 2/Bucles.java
@@ -2,7 +2,13 @@ public class Bucles {
     public static void main(String[] args) {
         // For(Incio; condicion de parada; incremento o decremento)
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Número de iteración = " + i );
+            System.out.println("Número de iteración = " + i);
+        }
+        // Estructura While
+        int j = 1;
+        while (j <= 5) {
+            System.out.println("Número de iteración J: " + j);
+            j++;
         }
     }
 }

02d684c1f9d370b07113006102da76b183c7fb05
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-02-20 23:55:53 +0000

    Creacion de arreglos y correción de ciertas cosas

---
+++ Clase 3/Arreglos.java
@@ -0,0 +1,13 @@
+public class Arreglos {
+    public static void main(String[] args) {
+        // Creación del arreglo de enteros
+        int[] a = { 3, 6, 5, 10, 20, 1, 4, 9 };
+
+        // Recorrer y mostrar los elementos del arreglos
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("En la posición[" + i + "]= " + a[i]);
+        }
+
+    }
+
+}

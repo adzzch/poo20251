b81c98ae2965dac7116bcdce2fdba112f4bee5dd
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-02-21 00:33:38 +0000

    ss

---
+++ Clase 3/Ejercicio13.java
@@ -0,0 +1,11 @@
+public class Ejercicio13 {
+
+    public static void main(String[] args) {
+        /*
+         * Dado un arreglo A de N elementos, almacenar los elementos mayores y menores
+         * que la media, almacenarlos en arreglos diferentes.
+         * 
+         * Análisis: 
+         */
+    }
+}

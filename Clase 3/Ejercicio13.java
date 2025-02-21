1d8ae1ab7df1af61f4b3ab14decdebd99acda2e1
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-02-21 00:56:23 +0000

    hola

--- Clase 3/Ejercicio13.java
+++ Clase 3/Ejercicio13.java
@@ -1,11 +1,20 @@
 public class Ejercicio13 {
 
     public static void main(String[] args) {
+
         /*
          * Dado un arreglo A de N elementos, almacenar los elementos mayores y menores
          * que la media, almacenarlos en arreglos diferentes.
          * 
-         * Análisis: 
+         * Análisis: Necesitamos crear el arreglo de tamaño aleatorio y generar números
+         * aleatorios para qye sean almacenados en el arreglo, posteriormente obtenemos
+         * la media del arreglo y comparamos cada posicion del arrelgo para determinar
+         * si s mayor o mejor a la media
          */
+        // n será el numero aleatorio para el tamaño del arreglo
+        int n = (int) Math.random() * (15 - 5 + 1) - 15;
+        // Declarar el arreglo
+        int[] a = new int[n];
+
     }
 }

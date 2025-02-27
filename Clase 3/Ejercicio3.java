ebcac89a5fec6ce8674c7503ceda0db22e895184
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-02-27 18:44:16 +0000

    Primera parte utilizando la estructura que utilizamos la clase pasada para que nos diera numeros aleatorios

--- Clase 3/Ejercicio3.java
+++ Clase 3/Ejercicio3.java
@@ -1,4 +1,4 @@
-public class Ejercicio3 {
+    public class Ejercicio3 {
     public static void main(String[] args) {
 
         /*
---
+++ Ejercicios Actividad/Ejercicio8.java
@@ -0,0 +1,16 @@
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        /* 
+         Elaborar un programa que lea 30 números y que imprima el número mayor
+         menor y el número de veces que se repiten ambos.
+         */
+        // Hacemos que nos den numeros random del 1 a 99 para asi llenar el arreglo
+        int numeros = (int) (Math.random() * 99) + 1;
+        int [] n = new int[30];
+        for (int i = 0; i < n.length; i++) {
+            
+        }
+    }   
+
+    
+}

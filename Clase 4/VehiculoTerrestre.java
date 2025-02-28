77bce177f32901b94d8ca3379c0c8760853e08a5
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-02-28 00:13:51 +0000

    Creación de atributos

---
+++ Clase 4/VehiculoTerrestre.java
@@ -0,0 +1,9 @@
+public class VehiculoTerrestre {
+    //Atributos de la clase o miembros
+    private String marca;
+    private String color;
+    private double cilindraje;
+    private String chasis;
+    private double peso;
+    private String potencia;
+}

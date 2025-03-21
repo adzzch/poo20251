2334b60b39ce2ca2f932a6250c13beb5dd6998f9
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-03-21 01:08:02 +0000

    Creacion de herencia

---
+++ Clase5/EjecutarHerencia.java
@@ -0,0 +1,20 @@
+package Clase5;
+
+public class EjecutarHerencia {
+    public static void main(String[] args) {
+
+        //Arreglo de trabajadores
+        Trabajador[] t = new Trabajador[3];
+
+        //Creacion de objetos (Instanciar)\
+        t[0] = new Operario(1, "Pepito", "Puertas", 135.0);
+        t[1] = new Consultor(2, "Juan", "Islas", 10);
+        t[2] = new Consultor(3, "Byron", "Cano", 20);
+
+        for(Trabajador x: t){
+            System.out.println(x.getClass() + " - " + x.pagar());
+        }
+        
+    }
+
+}

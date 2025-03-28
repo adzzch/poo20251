6ad609184f34330dde4e5120fd906db567e402ed
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-03-28 01:14:57 +0000

    correción

--- Clase6/Banco.java
+++ Clase6/Banco.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 public class Banco {
     private String nombre;
     private String direccion;
-    private Cuenta cuenta;
+    private Cuenta[] cuenta;
 
-    public Banco(String nombre, String direccion, Cuenta cuenta) {
+    public Banco(String nombre, String direccion, Cuenta[] cuenta) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.cuenta = cuenta;

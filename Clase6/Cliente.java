78bc59a0660b28236e6571d0abef8a692622bf5a
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-03-28 00:06:01 +0000

    Creacion clase "Cliente"

---
+++ Clase6/Cliente.java
@@ -0,0 +1,19 @@
+package Clase6;
+
+public class Cliente {
+
+    private long cedula;
+    private String nombre;
+
+    public Cliente(long cedula, String nombre){
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+
+    public String toString(){
+        return "Cliente { CC: " + cedula +
+                        " Nombre: " + nombre + "}";
+
+        }
+    }
+}

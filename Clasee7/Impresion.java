9b9550177a89c4f256b3586cd8e54664a32ab216
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-04-10 01:50:28 +0000

    se agrega metodo para imprimir todas las fotos

--- Clasee7/Impresion.java
+++ Clasee7/Impresion.java
@@ -2,8 +2,28 @@ package Clasee7;
 
 public class Impresion extends Producto {
     private String color;
-    private Foto[] foto; //Arreglo de fotos
+    private Foto[] foto;
 
-    //Metodos
+    public Impresion(int numero, String color, Foto[] fotos) {
+        super(numero);
+        this.color = color;
+        this.foto = fotos != null ? fotos : new Foto[0];
+    }
+    public void imprimirTodasFotos() {
+        for (Foto foto : foto) {
+            foto.print();
+        }
+    }
+    public Impresion(int numero, String color) {
+        this(numero, color, new Foto[0]);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Foto[] getFotos() {
+        return foto.clone();
+    }
 
 }

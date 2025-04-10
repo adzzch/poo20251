dfe6512fc4d77d42dea91ead3249e58ed91562e3
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-04-10 01:52:21 +0000

    Metodo para imprijmir fotos y string toString

--- Clasee7/Foto.java
+++ Clasee7/Foto.java
@@ -4,8 +4,15 @@ public class Foto {
 
     private String fichero;
 
+    public Foto(String fichero) {
+        this.fichero = fichero;
+    }
     //Metodos
     public void print(){
-        
+        System.out.println("Imprimiendo foto: " + fichero);
+    }
+    
+    public String toString() {
+        return "Foto {" + fichero + "}";
     }
 }

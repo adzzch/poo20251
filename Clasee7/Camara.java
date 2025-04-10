225bf8b0f64ec4bc5ddfdd9a44f4a4f4a7a4edde
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-04-10 02:22:29 +0000

    creacion camara

--- Clasee7/Camara.java
+++ Clasee7/Camara.java
@@ -5,15 +5,26 @@ public class Camara extends Producto {
     private String marca;
     private String modelo;
 
-    public Camara (String marca, String modelo){
+    public Camara(int numero, String marca, String modelo) {
+        super(numero);
         this.marca = marca;
         this.modelo = modelo;
     }
-    public String getMarca(){
+
+    // Getters básicos
+    public String getMarca() {
         return marca;
     }
-    public String getModelo(){
+
+    public String getModelo() {
         return modelo;
+    }
+
+    // toString() en tu estilo consistente
+    public String toString() {
+        return super.toString() + // Usa toString() de Producto
+               ", Marca: " + marca +
+               ", Modelo: " + modelo;
     }
 
 }

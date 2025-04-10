e51e414bca91cdb3579d747324681b5182322510
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-04-10 01:34:42 +0000

    creacionn de producto

--- Clasee7/Producto.java
+++ Clasee7/Producto.java
@@ -2,5 +2,12 @@ package Clasee7;
 
 public class Producto {
     private int numero;
-
+    
+    public Producto(int numero) {
+        this.numero = numero;
+    }
+    
+    public String toString() {
+        return "Producto {Número: " + numero + "}";
+    }
 }

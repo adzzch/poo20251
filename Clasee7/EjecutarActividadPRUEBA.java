f395dec1d603fa6f469f3cbb15012e562e646e7c
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-04-10 02:59:34 +0000

    Creacion del ejecutable

---
+++ Clasee7/EjecutarActividadPRUEBA.java
@@ -0,0 +1,13 @@
+package Clasee7;
+
+public class EjecutarActividadPRUEBA {
+    public static void main(String[] args) {
+
+        Cliente objCliente1 = new Cliente(1112042217, "Daniel Zamora");
+
+        Producto objProducto1 = new Producto(101);
+        Producto objProducto2 = new Producto(102);
+
+        
+    }
+}

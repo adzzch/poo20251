6ce2eefe15b8bb4fa22fba2989b05cefcbc2481c
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-04-24 23:41:38 +0000

    sss

--- Clasee7/Impresion.java
+++ Clasee7/Impresion.java
@@ -4,6 +4,7 @@ public class Impresion extends Producto {
     private String color;
     private Foto[] fotos;
 
+    // Herencia de la clase madre (Producto)
     public Impresion(String nombre, double precio, String color, Foto[] fotos) {
         super(nombre, precio, 102);
         this.color = color;

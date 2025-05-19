c03cee4bdea6239e5aecb38ac442327058a24579
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-05-19 06:17:38 +0000

    Creacion clase Personaje como clase padre de las proximas creaciones

---
+++ ProyectoJuegoPeleas/Personaje.java
@@ -0,0 +1,17 @@
+package ProyectoJuegoPeleas;
+
+public abstract class Personaje {
+    protected String nombre;
+    protected int puntosDeVida;
+    public Personaje(String nombre, int puntosDeVida) {
+        this.nombre = nombre;
+        this.puntosDeVida = puntosDeVida;
+    }
+    public abstract void atacar(Personaje oponente);
+    public void recibirDano(int dano) {
+        this.puntosDeVida -= dano;
+        if (this.puntosDeVida < 0) {
+            this.puntosDeVida = 0;
+        }
+    }
+}

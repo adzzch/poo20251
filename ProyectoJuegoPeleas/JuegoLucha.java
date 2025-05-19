fd776a7720f5c1f07f8fd5456368985ffc49f0cb
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-05-19 23:22:17 +0000

    Se crea el metodo para iniciar la pelea entre los dos jugadores

--- ProyectoJuegoPeleas/JuegoLucha.java
+++ ProyectoJuegoPeleas/JuegoLucha.java
@@ -8,4 +8,15 @@ public class JuegoLucha {
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
     }
+    public void iniciarPelea() {
+        System.out.println("La pelea comienza entre: " + jugador1.getNombre() + " y "
+        + jugador2.getNombre() + "...");
+        
+        while (jugador1.estaVivo() && jugador2.estaVivo()) {
+            turno(jugador1,jugador2);
+            if(jugador2.estaVivo()) {
+                turno(jugador2,jugador1);
+            }
+        }
+    }
 }

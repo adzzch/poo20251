aa08b75956b7b0d403d2ee5ca8416b1062143034
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-05-19 23:04:27 +0000

    Se crea el constructor y se le asignan los parametros jugador1 y jugador2

---
+++ ProyectoJuegoPeleas/JuegoLucha.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class JuegoLucha {
+    private Personaje jugador1;
+    private Personaje jugador2;
+
+    public JuegoLucha(Personaje jugador1, Personaje jugador2) {
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
+    }
+}

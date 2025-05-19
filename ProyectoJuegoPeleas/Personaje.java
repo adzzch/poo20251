c6a8073472799c001873b0855c960057d71c7ac5
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-05-19 06:19:07 +0000

    Damos formato

--- ProyectoJuegoPeleas/Personaje.java
+++ ProyectoJuegoPeleas/Personaje.java
@@ -3,11 +3,14 @@ package ProyectoJuegoPeleas;
 public abstract class Personaje {
     protected String nombre;
     protected int puntosDeVida;
+
     public Personaje(String nombre, int puntosDeVida) {
         this.nombre = nombre;
         this.puntosDeVida = puntosDeVida;
     }
+
     public abstract void atacar(Personaje oponente);
+
     public void recibirDano(int dano) {
         this.puntosDeVida -= dano;
         if (this.puntosDeVida < 0) {
@@ -15,13 +18,14 @@ public abstract class Personaje {
         }
     }
 
-
     public boolean estaVivo() {
         return this.puntosDeVida > 0;
     }
+
     public String getNombre() {
         return this.nombre;
     }
+
     public int getPuntosDeVida() {
         return this.puntosDeVida;
     }

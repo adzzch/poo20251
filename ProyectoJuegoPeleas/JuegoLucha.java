d8f11d9f6020268496d298913aaa911d964c4a78
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-05-20 00:19:31 +0000

    Final juego lucha

--- ProyectoJuegoPeleas/JuegoLucha.java
+++ ProyectoJuegoPeleas/JuegoLucha.java
@@ -8,15 +8,28 @@ public class JuegoLucha {
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
     }
+
     public void iniciarPelea() {
-        System.out.println("La pelea comienza entre: " + jugador1.getNombre() + " y "
-        + jugador2.getNombre() + "...");
-        
+        System.out.println("La pelea comienza entre " + jugador1.getNombre() + " y " + jugador2.getNombre() + " ...");
+
         while (jugador1.estaVivo() && jugador2.estaVivo()) {
-            turno(jugador1,jugador2);
-            if(jugador2.estaVivo()) {
-                turno(jugador2,jugador1);
+            turno(jugador1, jugador2);
+            if (jugador2.estaVivo()) {
+                turno(jugador2, jugador1);
             }
         }
+
+        // Mostrar el resultado de la pelea
+        if (jugador1.estaVivo()) {
+            System.out.println(jugador1.getNombre() + " ha ganado la pelea.");
+        } else {
+            System.out.println(jugador2.getNombre() + " ha ganado la pelea.");
+        }
+    }
+
+    private void turno(Personaje atacante, Personaje defensor) {
+        System.out.println("Turno de " + atacante.getNombre() + ". Puntos de vida de " + defensor.getNombre() + ": " + defensor.getPuntosDeVida());
+        atacante.atacar(defensor);
+        System.out.println(defensor.getNombre() + " ahora tiene " + defensor.getPuntosDeVida() + " puntos de vida.");
     }
 }

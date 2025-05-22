c3628104d05d2f6f2f7fd8444d24cee9dcc79763
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-05-22 20:31:16 +0000

    LiukANG

--- ProyectoJuegoPeleas/LiuKang.java
+++ ProyectoJuegoPeleas/LiuKang.java
@@ -33,7 +33,26 @@ public class LiuKang extends Personaje {
      */
 
     public int atacar(Personaje oponente) {
-        
+            Random rand = new Random();
+        int habilidadIndex = rand.nextInt(habilidades.length);
+        String habilidad = habilidades[habilidadIndex];
+        int dano = 0;
+
+        switch (habilidadIndex) {
+            case 0: // Patada Bicicleta
+                dano = 26;
+                break;
+            case 1: // Combo Shaolin
+                dano = 22;
+                break;
+            case 2: // Dragón de Fuego
+                dano = 38;
+                break;
+        }
+
+        oponente.recibirDano(dano);
+        System.out.println(this.nombre + " (Liu Kang) usa " + habilidad + " causando " + dano + " puntos de daño.");
+        return dano;
 
     }
 

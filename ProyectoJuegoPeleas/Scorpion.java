0e6e9ff26bab9d78159a32d313e54cb63a59a3e6
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-05-22 20:43:34 +0000

    formato

--- ProyectoJuegoPeleas/Scorpion.java
+++ ProyectoJuegoPeleas/Scorpion.java
@@ -1,13 +1,14 @@
 import java.util.Random;
+
 /**
  * Clase que representa a SubZero.
  * Tiene 3 habilidades y la última puede congelar al oponente un turno.
  */
 class SubZero extends Personaje {
     private final String[] habilidades = {
-        "Patada Deslizante - Daño: 16",
-        "Golpe de Hielo - Daño: 20",
-        "Bola de Hielo - Daño: 25 + congela 1 turno"
+            "Patada Deslizante - Daño: 16",
+            "Golpe de Hielo - Daño: 20",
+            "Bola de Hielo - Daño: 25 + congela 1 turno"
     };
 
     public SubZero(String nombre) {
@@ -26,8 +27,12 @@ class SubZero extends Personaje {
         int dano = 0;
 
         switch (indice) {
-            case 0: dano = 16; break;
-            case 1: dano = 20; break;
+            case 0:
+                dano = 16;
+                break;
+            case 1:
+                dano = 20;
+                break;
             case 2:
                 dano = 25;
                 System.out.println(oponente.getNombre() + " ha sido congelado y pierde su próximo turno.");

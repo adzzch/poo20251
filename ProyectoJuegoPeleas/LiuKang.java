77a25a157af3fbe5aed99a1a04d1de2e0d5e55e8
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-05-22 20:29:41 +0000

    Se añaden los comentarios de Raiden y se crea la clase LiuKang

--- ProyectoJuegoPeleas/LiuKang.java
+++ ProyectoJuegoPeleas/LiuKang.java
@@ -1,5 +1,11 @@
+/**
+ * Clase que representa a Liu Kang, un personaje del juego de lucha.
+ * Liu Kang tiene habilidades específicas que puede usar para atacar a su oponente.
+ */
+
 public class LiuKang extends Personaje {
-     private final String[] habilidadesLocal = {
+    // Arreglo que contiene las habilidades de Liu Kang con su daño correspondiente
+     private final String[] habilidades = {
         "Patada Bicicleta - Daño: 26",
         "Combo Shaolin - Daño: 22",
         "Dragón de Fuego - Daño: 38"
@@ -12,8 +18,25 @@ public class LiuKang extends Personaje {
             "Dragón de Fuego - Daño: 38"
         });
     }
-
+     /**
+     * Constructor de LiuKang.
+     * 
+     * para nombre El nombre del personaje.
+     */
     
+    /**
+     * Método que permite a Liu Kang atacar a un oponente.
+     * Selecciona aleatoriamente una habilidad y aplica el daño correspondiente.
+     * 
+     * para oponente El personaje que recibe el ataque.
+     * retorna el daño causado al oponente.
+     */
+
+    public int atacar(Personaje oponente) {
+        
+
+    }
+
 
 }
 
--- ProyectoJuegoPeleas/Raiden.java
+++ ProyectoJuegoPeleas/Raiden.java
@@ -1,29 +1,45 @@
 import java.util.Random;
 
+/**
+ * Clase que representa a Raiden.
+ * Tiene 3 habilidades de daño fijo.
+ */
 class Raiden extends Personaje {
     private final String[] habilidades = {
-        "Puños Eléctricos - Daño: 20",
-        "Patada Voladora - Daño: 24",
-        "Tormenta de Rayos - Daño: 35"
+            "Puños Eléctricos - Daño: 20",
+            "Patada Voladora - Daño: 24",
+            "Tormenta de Rayos - Daño: 35"
     };
+
     public Raiden(String nombre) {
         super(nombre, 110);
     }
-@Override
+
+    /**
+     * Ataca usando una habilidad aleatoria.
+     */
+    @Override
     public void atacar(Personaje oponente) {
         Random rand = new Random();
         int indice = rand.nextInt(habilidades.length);
         String habilidad = habilidades[indice];
         int dano = 0;
         switch (indice) {
-            case 0: dano = 20; break;
-            case 1: dano = 24; break;
-            case 2: dano = 35; break;
+            case 0:
+                dano = 20;
+                break;
+            case 1:
+                dano = 24;
+                break;
+            case 2:
+                dano = 35;
+                break;
         }
         oponente.recibirDano(dano);
         System.out.println(this.nombre + " (Raiden) usa " + habilidad + ", causando " + dano + " puntos de daño.");
     }
-@Override
+
+    @Override
     public String[] getHabilidades() {
         return habilidades.clone();
     }

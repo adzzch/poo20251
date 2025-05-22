b0e1e0592379662dad60f5334385b35760e6842a
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-05-22 20:09:09 +0000

    Se le da formato al documento y se añade comentarios para la explicación del mismo

--- ProyectoJuegoPeleas/Personaje.java
+++ ProyectoJuegoPeleas/Personaje.java
@@ -1,8 +1,11 @@
 import java.util.Random;
 import java.util.Scanner;
 
-
-public abstract class Personaje {
+/**
+ * Clase abstracta que representa un personaje en la lucha.
+ * Cada personaje tiene un nombre, puntos de vida y habilidades.
+ */
+abstract class Personaje {
     protected String nombre;
     protected int puntosDeVida;
 
@@ -11,16 +14,39 @@ public abstract class Personaje {
         this.puntosDeVida = puntosDeVida;
     }
 
+    /**
+     * Ejecuta un ataque contra el oponente.
+     * Selecciona una habilidad aleatoria y la usa.
+     * 
+     * @param oponente Otro personaje que recibe el ataque.
+     */
     public abstract void atacar(Personaje oponente);
+
+    /**
+     * Devuelve las descripciones de las habilidades del personaje.
+     * 
+     * @return Array con descripciones de habilidades.
+     */
     public abstract String[] getHabilidades();
 
+    /**
+     * Recibe daño, restando puntos de vida.
+     * Puntos de vida no bajan de 0.
+     * 
+     * @param dano Cantidad de daño a recibir.
+     */
     public void recibirDano(int dano) {
         this.puntosDeVida -= dano;
         if (this.puntosDeVida < 0) {
-            this.puntosDeVida = 0; // No se puede tener menos de 0 puntos de vida
+            this.puntosDeVida = 0;
         }
     }
 
+    /**
+     * Indica si el personaje sigue con vida (puntos de vida > 0).
+     * 
+     * @return true si está vivo.
+     */
     public boolean estaVivo() {
         return this.puntosDeVida > 0;
     }

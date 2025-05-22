f008324e6254aa4e94f961846b3bf6948393af3c
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-05-22 20:12:45 +0000

    Se crean las subclases de Personaje (LiuKang y Raiden)

---
+++ ProyectoJuegoPeleas/LiuKang.java
@@ -0,0 +1,19 @@
+public class LiuKang extends Personaje {
+     private final String[] habilidadesLocal = {
+        "Patada Bicicleta - Daño: 26",
+        "Combo Shaolin - Daño: 22",
+        "Dragón de Fuego - Daño: 38"
+    };
+
+    public LiuKang(String nombre) {
+        super(nombre, 90, new String[]{
+            "Patada Bicicleta - Daño: 26",
+            "Combo Shaolin - Daño: 22",
+            "Dragón de Fuego - Daño: 38"
+        });
+    }
+
+    
+
+}
+
---
+++ ProyectoJuegoPeleas/Raiden.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+class Raiden extends Personaje {
+    private final String[] habilidades = {
+        "Puños Eléctricos - Daño: 20",
+        "Patada Voladora - Daño: 24",
+        "Tormenta de Rayos - Daño: 35"
+    };
+}

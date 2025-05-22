3da000c84ec01ddbf0491dd141d106cf05568ca3
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-05-22 20:13:48 +0000

    se crea metodo atacar en la clase Raiden

--- ProyectoJuegoPeleas/Raiden.java
+++ ProyectoJuegoPeleas/Raiden.java
@@ -6,4 +6,24 @@ class Raiden extends Personaje {
         "Patada Voladora - Daño: 24",
         "Tormenta de Rayos - Daño: 35"
     };
+    public Raiden(String nombre) {
+        super(nombre, 110);
+    }
+@Override
+    public void atacar(Personaje oponente) {
+        Random rand = new Random();
+        int indice = rand.nextInt(habilidades.length);
+        String habilidad = habilidades[indice];
+        int dano = 0;
+        switch (indice) {
+            case 0: dano = 20; break;
+            case 1: dano = 24; break;
+            case 2: dano = 35; break;
+        }
+        oponente.recibirDano(dano);
+        System.out.println(this.nombre + " (Raiden) usa " + habilidad + ", causando " + dano + " puntos de daño.");
+    }
+
+
+
 }
